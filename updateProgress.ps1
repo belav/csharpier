b15cd60015d95085707feff02f90451beef4f0cb
belav <twobitbela@gmail.com>
2021-03-22 13:45:05 -0500

    cleanup

--- updateProgress.ps1
+++
@@ -1,8 +0,0 @@
-git checkout master
-git branch -d progress
-git checkout -b progress
-Copy-Item CSharpier/CSharpier.Core.Tests/Samples/AllInOne.cst CSharpier/CSharpier.Core.Tests/Samples/AllInOne.Formatted.cst
-git add CSharpier/CSharpier.Core.Tests/Samples/AllInOne.Formatted.cst
-git commit -m"Updating progress"
-git push --force --set-upstream origin progress --quiet
-git checkout master
