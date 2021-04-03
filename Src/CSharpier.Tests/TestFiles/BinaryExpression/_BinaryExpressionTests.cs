f777c3688cf4f7b4aff86c767b0654a1b3bd31a6
Lasath Fernando <devel@lasath.org>
2021-04-03 00:03:34 -0700

    Make string concatenations wrap
    Much like the bitwise operators, it makes sense to break around `+`
    because it's used for string concatenations.

---
+++ Src/CSharpier.Tests/TestFiles/BinaryExpression/_BinaryExpressionTests.cs
@@ -0,0 +1,14 @@
+using CSharpier.Tests.TestFileTests;
+using NUnit.Framework;
+
+namespace CSharpier.Tests.TestFiles
+{
+    public class BinaryExpressionTests : BaseTest
+    {
+        [Test]
+        public void StringConcatenation()
+        {
+            this.RunTest("BinaryExpression", "StringConcatenation");
+        }
+    }
+}
