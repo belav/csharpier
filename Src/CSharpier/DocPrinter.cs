a40d50948e91520c48e2e0ba40cd372a1af152de
belav <twobitbela@gmail.com>
2021-04-17 11:33:09 -0500

    fix stray semicolon which was breaking entry point

--- Src/CSharpier/DocPrinter.cs
+++ Src/CSharpier/DocPrinter.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
 using CSharpier.DocTypes;
-;
 
 namespace CSharpier
 {
