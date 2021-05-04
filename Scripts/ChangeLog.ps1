a176425f1c2a4640483ed8707b51201f4b908ca2
belav <twobitbela@gmail.com>
2021-05-03 21:47:49 -0500

    Updating for 0.9.1

---
+++ Scripts/ChangeLog.ps1
@@ -0,0 +1,31 @@
+﻿# this uses https://github.com/microsoft/PowerShellForGitHub
+# you'll need to Set-GitHubAuthentication first
+
+$versionNumber = "0.9.1"
+$previousVersionNumber = "0.9.0"
+
+$repository = "https://github.com/belav/csharpier"
+
+$milestones = Get-GitHubMilestone -Uri $repository -State "Open"
+$milestoneNumber = -1
+foreach($milestone in $milestones) {
+    if ($milestone.title -eq $versionNumber) {
+        $milestoneNumber = $milestone.number
+    }
+}
+
+$issues = Get-GitHubIssue -Uri https://github.com/belav/csharpier -MilestoneNumber $milestoneNumber -State All
+
+$content = [System.Text.StringBuilder]::new()
+$content.AppendLine("# " + $versionNumber)
+$content.AppendLine()
+$content.AppendLine("[diff](https://github.com/belav/csharpier/compare/" + $previousVersionNumber + "..." + $versionNumber + ")")
+$content.AppendLine()
+foreach ($issue in $issues) {
+    if ($issue.title.ToLower().Contains("checklist")) {
+        continue
+    }
+    $content.AppendLine("- " + $issue.title + " [#" + $issue.number +"](" + $issue.url + ")")
+}
+
+Write-Output $content.ToString()
