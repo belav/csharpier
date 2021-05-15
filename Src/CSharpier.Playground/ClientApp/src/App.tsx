0f9bc9755a9467fb50a67522c845aa43714f2427
belav <twobitbela@gmail.com>
2021-05-15 12:01:54 -0500

    little tweak

--- Src/CSharpier.Playground/ClientApp/src/App.tsx
+++ Src/CSharpier.Playground/ClientApp/src/App.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { Layout } from "./Layout";
 
 export const App = () => {
-    
     return (
         <AppContext.Provider value={useSetupAppContext()}>
             <Layout />
