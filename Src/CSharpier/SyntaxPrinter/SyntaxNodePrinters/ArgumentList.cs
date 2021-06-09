3fc929a092c495f253e5c88317ac9cc69178b6f9
Divyansh Shukla <divyanshshukla13@gmail.com>
2021-06-09 16:05:32 +0200

    Fix breaking in constructors when creating object

--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/ArgumentList.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/ArgumentList.cs
@@ -7,16 +7,13 @@ namespace CSharpier.SyntaxPrinter.SyntaxNodePrinters
     {
         public static Doc Print(ArgumentListSyntax node)
         {
-            return node.Parent
-                is not ObjectCreationExpressionSyntax
-                ? Doc.Group(
-                        ArgumentListLike.Print(
-                            node.OpenParenToken,
-                            node.Arguments,
-                            node.CloseParenToken
-                        )
-                    )
-                : ArgumentListLike.Print(node.OpenParenToken, node.Arguments, node.CloseParenToken);
-        }
+            return Doc.Group(
+                ArgumentListLike.Print(
+                    node.OpenParenToken,
+                    node.Arguments,
+                    node.CloseParenToken
+                )
+            );
+    }
     }
 }
