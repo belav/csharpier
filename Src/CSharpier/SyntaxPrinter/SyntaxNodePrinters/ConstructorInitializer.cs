b5cd65a638048183ea3f2d6d35033e4738a24030
Divyansh Shukla <divyanshshukla13@gmail.com>
2021-06-12 15:26:42 +0200

    Fix comment handling

--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/ConstructorInitializer.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/ConstructorInitializer.cs
@@ -19,9 +19,12 @@ namespace CSharpier.SyntaxPrinter.SyntaxNodePrinters
 
         private static Doc Print(ConstructorInitializerSyntax node, string? groupId)
         {
+            var colonToken = Token.PrintWithSuffix(node.ColonToken, " ");
             return Doc.Group(
                 Doc.Indent(groupId != null ? Doc.IfBreak(" ", Doc.Line, groupId) : Doc.Line),
-                Token.PrintWithSuffix(node.ColonToken, " "),
+                groupId != null
+                    ? Doc.IfBreak(Doc.Align(2, colonToken), Doc.Indent(colonToken), groupId)
+                    : Doc.Indent(colonToken),
                 Token.Print(node.ThisOrBaseKeyword),
                 groupId != null
                     ? ArgumentList.Print(node.ArgumentList)
