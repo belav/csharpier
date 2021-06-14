f9163627df470f0e180c2fd9d78f9a8e8cc8a702
belav <twobitbela@gmail.com>
2021-06-14 13:59:44 -0500

    Fixing bug with empty array ranks breaking
    closes #301

--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/ArrayRankSpecifier.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/ArrayRankSpecifier.cs
@@ -9,6 +9,15 @@ namespace CSharpier.SyntaxPrinter.SyntaxNodePrinters
     {
         public static Doc Print(ArrayRankSpecifierSyntax node)
         {
+            if (node.Sizes.All(o => o is OmittedArraySizeExpressionSyntax))
+            {
+                return Doc.Concat(
+                    Token.Print(node.OpenBracketToken),
+                    SeparatedSyntaxList.Print(node.Sizes, Node.Print, Doc.Null),
+                    Token.Print(node.CloseBracketToken)
+                );
+            }
+
             return Doc.Group(
                 Token.Print(node.OpenBracketToken),
                 node.Sizes.Any()
