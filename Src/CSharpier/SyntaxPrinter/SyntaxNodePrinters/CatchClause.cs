cee9d98b2db47018215eb2e8c725d7a35b732a63
belav <twobitbela@gmail.com>
2021-06-14 14:42:15 -0500

    Improved logic for where to break long catch clauses
    closes #86

--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/CatchClause.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/CatchClause.cs
@@ -10,33 +10,34 @@ namespace CSharpier.SyntaxPrinter.SyntaxNodePrinters
     {
         public static Doc Print(CatchClauseSyntax node)
         {
-            var docs = new List<Doc>();
-            docs.Add(Token.Print(node.CatchKeyword));
-            if (node.Declaration != null)
-            {
-                docs.Add(
-                    " ",
-                    Token.Print(node.Declaration.OpenParenToken),
-                    Node.Print(node.Declaration.Type),
-                    node.Declaration.Identifier.RawKind != 0 ? " " : Doc.Null,
-                    Token.Print(node.Declaration.Identifier),
-                    Token.Print(node.Declaration.CloseParenToken)
-                );
-            }
-
-            if (node.Filter != null)
-            {
-                docs.Add(
-                    " ",
-                    Token.PrintWithSuffix(node.Filter.WhenKeyword, " "),
-                    Token.Print(node.Filter.OpenParenToken),
-                    Node.Print(node.Filter.FilterExpression),
-                    Token.Print(node.Filter.CloseParenToken)
-                );
-            }
-
-            docs.Add(Block.Print(node.Block));
-            return Doc.Concat(docs);
+            return Doc.Concat(
+                Token.Print(node.CatchKeyword),
+                Doc.Group(
+                    node.Declaration != null
+                        ? Doc.Concat(
+                                " ",
+                                Token.Print(node.Declaration.OpenParenToken),
+                                Node.Print(node.Declaration.Type),
+                                node.Declaration.Identifier.RawKind != 0 ? " " : Doc.Null,
+                                Token.Print(node.Declaration.Identifier),
+                                Token.Print(node.Declaration.CloseParenToken)
+                            )
+                        : Doc.Null,
+                    node.Filter != null
+                        ? Doc.Indent(
+                                Doc.Line,
+                                Token.PrintWithSuffix(node.Filter.WhenKeyword, " "),
+                                Token.Print(node.Filter.OpenParenToken),
+                                Doc.Group(
+                                    Doc.Indent(Node.Print(node.Filter.FilterExpression)),
+                                    Doc.SoftLine
+                                ),
+                                Token.Print(node.Filter.CloseParenToken)
+                            )
+                        : Doc.Null
+                ),
+                Block.Print(node.Block)
+            );
         }
     }
 }
