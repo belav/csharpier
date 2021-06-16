1cc3a5ffd7061900f53a07a120131c9ccab362bd
Bela VanderVoort <twobitbela@gmail.com>
2021-06-16 17:18:26 -0500

    Always break + indent with optional braces (#306)
    closes #303
    closes #245
    closes #255

---
+++ Src/CSharpier/SyntaxPrinter/OptionalBraces.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+using CSharpier.DocTypes;
+using CSharpier.SyntaxPrinter.SyntaxNodePrinters;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace CSharpier.SyntaxPrinter
+{
+    public static class OptionalBraces
+    {
+        public static Doc Print(StatementSyntax node, string? groupId = null)
+        {
+            return node
+                is BlockSyntax blockSyntax
+                ? groupId == null
+                        ? Block.Print(blockSyntax)
+                        : Block.PrintWithConditionalSpace(blockSyntax, groupId)
+                : Doc.Indent(Doc.HardLine, Node.Print(node));
+        }
+    }
+}
