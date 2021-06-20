726c6bf3cda92b0c50fe1a04a131d1c2228ebfbd
Divyansh Shukla <divyanshshukla13@gmail.com>
2021-06-20 11:28:50 +0200

    Refactor RightHandSide.cs to use switch expression

--- Src/CSharpier/SyntaxPrinter/RightHandSide.cs
+++ Src/CSharpier/SyntaxPrinter/RightHandSide.cs
@@ -9,66 +9,44 @@ namespace CSharpier.SyntaxPrinter
     {
         public static Doc Print(ExpressionSyntax node)
         {
-            if(node.Kind() is SyntaxKind.CollectionInitializerExpression)
-            {
-                return Doc.Group(Doc.Line, Node.Print(node));
-            }
-            var formatMode = node
-                is AnonymousObjectCreationExpressionSyntax
-                    or AnonymousMethodExpressionSyntax
-                    or InitializerExpressionSyntax
-                    or InvocationExpressionSyntax
-                    or ConditionalExpressionSyntax
-                    or ObjectCreationExpressionSyntax
-                    or SwitchExpressionSyntax
-                    or LambdaExpressionSyntax
-                    or AwaitExpressionSyntax
-                    or WithExpressionSyntax
-                ? FormatMode.NoIndent
-                : node
-                        is BinaryExpressionSyntax
-                            or InterpolatedStringExpressionSyntax
-                            or IsPatternExpressionSyntax
-                            or LiteralExpressionSyntax
-                            or QueryExpressionSyntax
-                            or StackAllocArrayCreationExpressionSyntax
-                        ? FormatMode.IndentWithLine
-                        : FormatMode.Indent;
             var groupId = Guid.NewGuid().ToString();
-            return Doc.Group(
-                formatMode is FormatMode.IndentWithLine
-                    ? Doc.Null
-                    : Doc.GroupWithId(groupId, Doc.Indent(Doc.Line)),
-                node
-                    is InvocationExpressionSyntax
-                        or ParenthesizedLambdaExpressionSyntax
-                        or ObjectCreationExpressionSyntax
-                        or ElementAccessExpressionSyntax
-                        or ArrayCreationExpressionSyntax
-                        or ImplicitArrayCreationExpressionSyntax
-                    ? Doc.IndentIfBreak(Doc.Group(Node.Print(node)), groupId)
-                    : Doc.Group(IndentIfNeeded(node, formatMode))
-            );
-        }
-
-        private static Doc IndentIfNeeded(ExpressionSyntax initializerValue, FormatMode formatMode)
-        {
-            if (formatMode is FormatMode.NoIndent)
+            return node switch
             {
-                return Node.Print(initializerValue);
-            }
-
-            return Doc.Indent(
-                formatMode is FormatMode.IndentWithLine ? Doc.Line : Doc.Null,
-                Node.Print(initializerValue)
-            );
-        }
-
-        private enum FormatMode
-        {
-            NoIndent,
-            IndentWithLine,
-            Indent
+                InvocationExpressionSyntax
+                or ParenthesizedLambdaExpressionSyntax
+                or ObjectCreationExpressionSyntax
+                or ElementAccessExpressionSyntax
+                or ArrayCreationExpressionSyntax
+                or ImplicitArrayCreationExpressionSyntax => Doc.Group(
+                    Doc.GroupWithId(groupId, Doc.Indent(Doc.Line)),
+                    Doc.IndentIfBreak(Doc.Group(Node.Print(node)), groupId)
+                ),
+                InitializerExpressionSyntax when node.Kind()
+                is SyntaxKind.CollectionInitializerExpression => Doc.Group(
+                    Doc.Line,
+                    Node.Print(node)
+                ),
+                AnonymousObjectCreationExpressionSyntax
+                or AnonymousMethodExpressionSyntax
+                or InitializerExpressionSyntax
+                or ConditionalExpressionSyntax
+                or SwitchExpressionSyntax
+                or LambdaExpressionSyntax
+                or AwaitExpressionSyntax
+                or WithExpressionSyntax => Doc.Group(
+                    Doc.Group(Doc.Indent(Doc.Line)),
+                    Node.Print(node)
+                ),
+                BinaryExpressionSyntax
+                or InterpolatedStringExpressionSyntax
+                or IsPatternExpressionSyntax
+                or LiteralExpressionSyntax
+                or QueryExpressionSyntax
+                or StackAllocArrayCreationExpressionSyntax => Doc.Indent(
+                    Doc.Group(Doc.Line, Node.Print(node))
+                ),
+                _ => Doc.Group(Doc.Indent(Doc.Line), Doc.Indent(Node.Print(node)))
+            };
         }
     }
 }
