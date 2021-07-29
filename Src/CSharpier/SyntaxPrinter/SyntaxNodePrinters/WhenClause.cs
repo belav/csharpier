67eb5cfb5979b0ff85bea9ef605a1026a28582fe
Phil McCloghry-Laing <phil@philistine.com.au>
2021-07-29 15:41:09 +1000

    Use Group instead of GroupWithId

--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/WhenClause.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/WhenClause.cs
@@ -1,4 +1,3 @@
-using System;
 using CSharpier.DocTypes;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
@@ -8,9 +7,7 @@ namespace CSharpier.SyntaxPrinter.SyntaxNodePrinters
     {
         public static Doc Print(WhenClauseSyntax node)
         {
-            var groupId = Guid.NewGuid().ToString();
-            return Doc.GroupWithId(
-                groupId,
+            return Doc.Group(
                 Doc.Indent(
                     Doc.Line,
                     Token.PrintWithSuffix(node.WhenKeyword, " "),
