2673abb8f8b764fd54c3259c58659283792f0104
belav <twobitbela@gmail.com>
2021-09-20 11:36:24 -0500

    Handle fixed statement with no braces

--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/FixedStatement.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/FixedStatement.cs
@@ -28,7 +28,10 @@ namespace CSharpier.SyntaxPrinter.SyntaxNodePrinters
                 ),
                 node.Statement is BlockSyntax blockSyntax
                     ? Block.PrintWithConditionalSpace(blockSyntax, groupId)
-                    : Doc.Concat(Doc.HardLine, Node.Print(node.Statement))
+                    : Doc.IndentIf(
+                          node.Statement is not FixedStatementSyntax,
+                          Doc.Concat(Doc.HardLine, Node.Print(node.Statement))
+                      )
             );
         }
     }
