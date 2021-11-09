d56198fbad0c3ddf6f838ae165043a58d546a524
Bela VanderVoort <twobitbela@gmail.com>
2021-11-09 08:34:35 -0600

    Cleaning up comments, target 5 and 6

--- Scripts/Helpers.ps1
+++ Scripts/Helpers.ps1
@@ -1,6 +1,5 @@
 $repositoryRoot = Join-Path $PSScriptRoot ".."
 $csProjectPath = Join-Path $repositoryRoot "Src/CSharpier.Cli/CSharpier.Cli.csproj"
-# TODO 10 should we test multiple versions? We should probably not only target .net6
 $csharpierDllPath = Join-Path $repositoryRoot "Src/CSharpier.Cli/bin/release/net6.0/dotnet-csharpier.dll"
 
 function Build-CSharpier
