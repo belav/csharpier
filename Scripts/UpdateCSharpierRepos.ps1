e906bdf6c0e105cc5ccb84b592bcc4ca5383ea27
Bela VanderVoort <bela.vandervoort@optimizely.com>
2021-11-15 13:56:10 -0600

    Fixing issue with file scoped namespaces (#480)

---
+++ Scripts/UpdateCSharpierRepos.ps1
@@ -0,0 +1,48 @@
+$repositories = @()
+$repositories += "https://github.com/dotnet/aspnetcore.git"
+$repositories += "https://github.com/aspnet/AspNetWebStack.git"
+$repositories += "https://github.com/AutoMapper/AutoMapper.git"
+$repositories += "https://github.com/castleproject/Core.git"
+$repositories += "https://github.com/dotnet/command-line-api.git"
+$repositories += "https://github.com/dotnet/format.git"
+$repositories += "https://github.com/dotnet/efcore.git"
+$repositories += "https://github.com/moq/moq4.git"
+$repositories += "https://github.com/JamesNK/Newtonsoft.Json.git"
+$repositories += "https://github.com/dotnet/roslyn.git"
+$repositories += "https://github.com/dotnet/runtime.git"
+
+$tempLocation = "c:\temp\UpdateRepos"
+
+#TODO just pull it they exist already
+Remove-Item -Recurse -Force $tempLocation
+
+New-Item $tempLocation -Force -ItemType Directory
+Set-Location $tempLocation
+
+$ErrorActionPreference = "Continue"
+
+foreach ($repository in $repositories)
+{
+    & git clone $repository
+}
+
+$destination = "C:\projects\csharpier-repos\"
+
+Get-ChildItem $tempLocation | Copy-Item -Destination $destination -Filter *.cs -Recurse -Force
+
+$items = Get-ChildItem -Recurse C:\projects\csharpier-repos -File
+foreach ($item in $items) {
+    if ($item.Name -eq ".git") {
+        Remove-Item -Force -Recurse $item.FullName
+    }
+    if ($item.Extension -ne ".cs") {
+        Remove-Item $item.FullName
+    }
+}
+
+$items = Get-ChildItem C:\projects\csharpier-repos -Directory
+foreach ($item in $items) {
+    if ($item.Name -eq ".git") {
+        Remove-Item -Force -Recurse $item.FullName
+    }
+}
--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/FileScopedNamespaceDeclaration.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/FileScopedNamespaceDeclaration.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
 using CSharpier.DocTypes;
+using CSharpier.Utilities;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace CSharpier.SyntaxPrinter.SyntaxNodePrinters
@@ -7,7 +10,8 @@ namespace CSharpier.SyntaxPrinter.SyntaxNodePrinters
     {
         public static Doc Print(FileScopedNamespaceDeclarationSyntax node)
         {
-            return Doc.Concat(
+            var docs = new List<Doc>()
+            {
                 ExtraNewLines.Print(node),
                 AttributeLists.Print(node, node.AttributeLists),
                 Modifiers.Print(node.Modifiers),
@@ -15,7 +19,26 @@ namespace CSharpier.SyntaxPrinter.SyntaxNodePrinters
                 " ",
                 Node.Print(node.Name),
                 Token.Print(node.SemicolonToken)
-            );
+            };
+
+            docs.Add(Doc.HardLine);
+
+            if (node.Externs.Any())
+            {
+                docs.Add(Doc.Join(Doc.HardLine, node.Externs.Select(Node.Print)), Doc.HardLine);
+            }
+
+            if (node.Usings.Any())
+            {
+                docs.Add(Doc.Join(Doc.HardLine, node.Usings.Select(Node.Print)), Doc.HardLine);
+            }
+
+            if (node.Members.Any())
+            {
+                docs.Add(Doc.Join(Doc.HardLine, node.Members.Select(Node.Print)), Doc.HardLine);
+            }
+
+            return Doc.Concat(docs);
         }
     }
 }
