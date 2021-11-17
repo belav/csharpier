3f40c0110e8b205f376b8ae6fbcda1742bbddb94
Bela VanderVoort <twobitbela@gmail.com>
2021-11-16 18:35:45 -0600

    Fixing up merge conflict

--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/FileScopedNamespaceDeclaration.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/FileScopedNamespaceDeclaration.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using CSharpier.DocTypes;
+using CSharpier.SyntaxPrinter;
 using CSharpier.Utilities;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
@@ -12,35 +13,20 @@ internal static class FileScopedNamespaceDeclaration
     {
         var docs = new List<Doc>()
         {
-            var docs = new List<Doc>()
-            {
-                ExtraNewLines.Print(node),
-                AttributeLists.Print(node, node.AttributeLists),
-                Modifiers.Print(node.Modifiers),
-                Token.Print(node.NamespaceKeyword),
-                " ",
-                Node.Print(node.Name),
-                Token.Print(node.SemicolonToken)
-            };
+            ExtraNewLines.Print(node),
+            AttributeLists.Print(node, node.AttributeLists),
+            Modifiers.Print(node.Modifiers),
+            Token.Print(node.NamespaceKeyword),
+            " ",
+            Node.Print(node.Name),
+            Token.Print(node.SemicolonToken)
+        };
 
-            docs.Add(Doc.HardLine);
+        docs.Add(Doc.HardLine);
 
-            if (node.Externs.Any())
-            {
-                docs.Add(Doc.Join(Doc.HardLine, node.Externs.Select(Node.Print)), Doc.HardLine);
-            }
-
-            if (node.Usings.Any())
-            {
-                docs.Add(Doc.Join(Doc.HardLine, node.Usings.Select(Node.Print)), Doc.HardLine);
-            }
-
-            if (node.Members.Any())
-            {
-                docs.Add(Doc.Join(Doc.HardLine, node.Members.Select(Node.Print)), Doc.HardLine);
-            }
-
-            return Doc.Concat(docs);
+        if (node.Externs.Any())
+        {
+            docs.Add(Doc.Join(Doc.HardLine, node.Externs.Select(Node.Print)), Doc.HardLine);
         }
 
         if (node.Usings.Any())
