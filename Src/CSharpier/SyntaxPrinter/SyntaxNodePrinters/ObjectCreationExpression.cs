5331bb94861fa6e9f758c9a642cced40a3987afb
Bela VanderVoort <twobitbela@gmail.com>
2021-11-21 10:41:15 -0600

    Break nested initializers
    closes #487

--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/ObjectCreationExpression.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/ObjectCreationExpression.cs
@@ -4,21 +4,36 @@ internal static class ObjectCreationExpression
 {
     public static Doc Print(ObjectCreationExpressionSyntax node)
     {
-        return Doc.Group(
-            Token.PrintWithSuffix(node.NewKeyword, " "),
-            Node.Print(node.Type),
-            node.ArgumentList != null
-              ? Doc.Group(
-                    ArgumentListLike.Print(
-                        node.ArgumentList.OpenParenToken,
-                        node.ArgumentList.Arguments,
-                        node.ArgumentList.CloseParenToken
+        return BreakParentIfNested(
+            node,
+            Doc.Group(
+                Token.PrintWithSuffix(node.NewKeyword, " "),
+                Node.Print(node.Type),
+                node.ArgumentList != null
+                  ? Doc.Group(
+                        ArgumentListLike.Print(
+                            node.ArgumentList.OpenParenToken,
+                            node.ArgumentList.Arguments,
+                            node.ArgumentList.CloseParenToken
+                        )
                     )
-                )
-              : Doc.Null,
-            node.Initializer != null
-              ? Doc.Concat(Doc.Line, InitializerExpression.Print(node.Initializer))
-              : Doc.Null
+                  : Doc.Null,
+                node.Initializer != null
+                  ? Doc.Concat(Doc.Line, InitializerExpression.Print(node.Initializer))
+                  : Doc.Null
+            )
         );
+    }
+
+    public static Doc BreakParentIfNested(BaseObjectCreationExpressionSyntax node, Doc doc)
+    {
+        return
+            (
+                node.Parent?.Parent is InitializerExpressionSyntax
+                || node.Parent is InitializerExpressionSyntax
+            )
+            && node.Initializer != null
+          ? Doc.Concat(doc, Doc.BreakParent)
+          : doc;
     }
 }
