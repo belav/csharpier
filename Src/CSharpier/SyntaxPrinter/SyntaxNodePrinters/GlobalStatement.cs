6597401a232c905067eacd75977be7e64c782da5
Bela VanderVoort <bela.vandervoort@optimizely.com>
2021-12-01 17:47:48 -0600

    Fixing extra newlines in global statements (#502)
    closes #501

--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/GlobalStatement.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/GlobalStatement.cs
@@ -5,7 +5,6 @@ internal static class GlobalStatement
     public static Doc Print(GlobalStatementSyntax node)
     {
         return Doc.Concat(
-            ExtraNewLines.Print(node),
             AttributeLists.Print(node, node.AttributeLists),
             Modifiers.Print(node.Modifiers),
             Node.Print(node.Statement)
