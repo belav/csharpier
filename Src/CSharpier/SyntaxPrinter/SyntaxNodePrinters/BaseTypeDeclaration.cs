8d54bff395e7e1e5e6cf575c8bcc4c92763f5bb3
Bela VanderVoort <twobitbela@gmail.com>
2022-01-01 14:23:39 -0600

    Adding more edge cases that need to be fixed.

--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/BaseTypeDeclaration.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/BaseTypeDeclaration.cs
@@ -1,20 +1,92 @@
 namespace CSharpier.SyntaxPrinter.SyntaxNodePrinters;
 
-// TODO what about regions and other directives
+// TODO these are all the edge cases left + any failing tests
 /*
- 
- // the two blank lines got addes
- 
-using System.Collections.Generic;
-using System.Globalization;
+
+// this loses the new line before the namespace
+
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Reflection;
+
 #if ASPNETWEBAPI
-using System.Net.Http;
-
+namespace System.Web.Http.Internal
 #else
-using System.Web.Routing;
-
+namespace System.Web.WebPages
 #endif
- 
+
+// this adds new lines
+public class ClassName
+{
+    #if ASPNETWEBAPI
+            public IEnumerable<IHttpRouteConstraint> Constraints { get; private set; }
+    #else
+            public IEnumerable<IRouteConstraint> Constraints { get; private set; }
+    #endif
+
+    public void DoStuff() { }
+}
+
+// this adds new lines too
+public class ClassName
+{
+#if !ASPNETWEBAPI
+        private readonly string _controllerName;
+#endif
+
+#if ASPNETWEBAPI
+        private readonly string _prefix;
+#else
+        private readonly string _areaPrefix;
+        private readonly string _controllerPrefix;
+#endif
+}
+
+// this completely loses the break
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage(
+    "Microsoft.Design",
+    "CA2210:AssembliesShouldHaveValidStrongNames",
+    Justification = "Assembly is delay-signed"
+)]
+
+// this loses a new line before the comment
+
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Web;
+using Microsoft.Web.Helpers;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Microsoft.Web.Helpers")]
+[assembly: AssemblyDescription("")]
+[assembly: PreApplicationStartMethod(typeof(PreApplicationStartCode), "Start")]
+[assembly: InternalsVisibleTo(
+    "Microsoft.Web.Helpers.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b5fc90e7027f67871e773a8fde8938c81dd402ba65b9201d60593e96c492651e889cc13f1415ebb53fac1131ae0bd333c5ee6021672d9718ea31a8aebd0da0072f25d87dba6fc90ffd598ed4da35e44c398c454307e8e33b8426143daec9f596836f97c8f74750e5975c64e2189f45def46b2a2b1247adc3652bf5c308055da9"
+)]
+
+// this loses the new line before pragma
+
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+
+#pragma warning disable 659 // overrides AddToHashCodeCombiner instead
+
+namespace Microsoft.Web.Mvc.ExpressionUtil
+{ }
+
  */
 
 internal static class BaseTypeDeclaration
