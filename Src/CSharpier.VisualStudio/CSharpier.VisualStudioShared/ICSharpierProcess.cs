18046be781a8c482d9cc64d9635e937a08f92170
Bela VanderVoort <twobitbela@gmail.com>
2022-01-23 18:30:57 -0600

    Minor refactoring

--- Src/CSharpier.VisualStudio/CSharpier.VisualStudioShared/ICSharpierProcess.cs
+++ Src/CSharpier.VisualStudio/CSharpier.VisualStudioShared/ICSharpierProcess.cs
@@ -2,7 +2,6 @@ namespace CSharpier.VisualStudio
 {
     public interface ICSharpierProcess
     {
-        bool CanFormat { get; }
         string FormatFile(string content, string fileName);
     }
 }
