bccd3590591438ce913c528b19efeadbb8c78cc2
Bela VanderVoort <bela.vandervoort@optimizely.com>
2022-01-28 17:05:12 -0600

    Little things

--- Src/CSharpier.Rider/CHANGELOG.md
+++ Src/CSharpier.Rider/CHANGELOG.md
@@ -2,7 +2,7 @@
 
 # csharpier-rider Changelog
 
-## [Unreleased]
+## [1.2.0]
 - Fix bug with dotnet csharpier sometimes outputting .net welcome message
 - Support for using local version of csharpier from tool manifest file
 - Run csharpier from custom tools location so that the global/local versions of the tool are not locked.
--- Src/CSharpier.Rider/src/main/java/com/intellij/csharpier/ReformatWithCSharpierOnSave3.java
+++ Src/CSharpier.Rider/src/main/java/com/intellij/csharpier/ReformatWithCSharpierOnSave3.java
@@ -1,7 +1,5 @@
 package com.intellij.csharpier;
 
-import com.intellij.ide.actions.SaveAllAction;
-import com.intellij.ide.actions.SaveDocumentAction;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.diagnostic.Logger;
@@ -11,18 +9,9 @@ import com.intellij.openapi.fileEditor.FileDocumentManagerListener;
 import com.intellij.openapi.project.ProjectLocator;
 import org.jetbrains.annotations.NotNull;
 
-// TODO try the XamlStlerComponent thing
-/*
-I have a FileDocumentManagerListener implemented in my rider plugin.
-When I launch it into Rider to test, beforeDocumentSaving fires any time I save a file. Along with beforeAllDocumentsSaving
-When I build the plugin, and install it in Rider, beforeDocumentSaving fires, but beforeAllDocumentsSaving only fires when rider loses focus.
-Probably related, if I implement an ActionsOnSaveFileDocumentManagerListener.ActionOnSave, it works when launching rider to test the plugin, but installing the built plugin in rider it does not work.
-Is there some reason rider is behaving differently when I launch it directly vs launching it via run plugin?
-
-And maybe a simpler question, is there a recommended way in rider to modify a file on save? I am digging into all of this because a "move to new class" refactor causes the following exception "Someone is intentionally modifying the document while syncing from backend."
-When I modify the document, I use WriteCommandAction.runWriteCommandAction but still get the exception.
-If I run all of my formatting logic in ApplicationManager.getApplication().invokeLater then I avoid the exception, but the built plugin ends up formatting it twice, because beforeDocumentSaving will get fired twice. But it will only fire once when I am testing the plugin through intellij.
- */
+// TODO read up on the slack channel stuff
+// TODO optionally, figure out a way to not hit format twice.
+// TODO but more importantly, get out a VS update
 
 public class ReformatWithCSharpierOnSave3 implements FileDocumentManagerListener {
     private final Logger logger = CSharpierLogger.getInstance();
