afc5534ad049eda1d594fdd88a768842ba65542d
Juan Sanchez <11725911+JSanchezIO@users.noreply.github.com>
2022-02-19 23:50:02 -0600

    fix: macos rider (#616)
    * fix: macos rider
    
    * refactor: only run commands in shell if os is mac

--- Src/CSharpier.Rider/src/main/java/com/intellij/csharpier/ProcessHelper.java
+++ Src/CSharpier.Rider/src/main/java/com/intellij/csharpier/ProcessHelper.java
@@ -1,6 +1,8 @@
 package com.intellij.csharpier;
 
+import org.apache.commons.lang.SystemUtils;
 import java.io.*;
+import java.util.HashMap;
 import java.util.Map;
 
 public class ProcessHelper {
@@ -12,10 +14,18 @@ public class ProcessHelper {
 
             logger.debug("user.dir is " + System.getProperty("user.dir"));
             logger.debug("Running " + String.join(" ", command) + directoryToLog);
-            var processBuilder = new ProcessBuilder(command);
-            if (env != null) {
-                processBuilder.environment().putAll(env);
+            var processBuilder = new ProcessBuilder(GetShellCommandIfNeeded(command));
+
+            if (env == null) {
+                env = new HashMap<>();
             }
+
+            if (!env.containsKey("PATH")) {
+                env.put("PATH", GetPathWithDotNetBinary());
+            }
+
+            processBuilder.environment().putAll(env);
+
             if (workingDirectory != null) {
                 processBuilder.directory(workingDirectory);
             }
@@ -46,5 +56,30 @@ public class ProcessHelper {
 
         }
         return null;
+    }
+
+    private static String GetPathWithDotNetBinary() {
+        var path = System.getenv("PATH");
+
+        // For Mac, the .NET binary isn't available for ProcessBuilder, so we'll add the default
+        // installation location to the PATH. We'll prefer the ARM version and fallback to the x64.
+        if (SystemUtils.IS_OS_MAC) {
+            return path + ":/usr/local/share/dotnet:/usr/local/share/dotnet/x64/dotnet";
+        }
+
+        // For others, it seems the .NET binary is already available to ProcessBuilder by default.
+        // So we'll just return the PATH as is.
+        return path;
+    }
+
+    // For Mac, we'll have updated the PATH to include the .NET binary. However, setting
+    // the PATH doesn't affect the ProcessBuilder's command, but it will apply to
+    // spawned processes, so we'll run the desired command in the OS's default shell.
+    private static String[] GetShellCommandIfNeeded(String[] command) {
+        if (SystemUtils.IS_OS_MAC) {
+            return new String[] {"/bin/zsh", "-c", String.join(" ", command)};
+        }
+
+        return command;
     }
 }
