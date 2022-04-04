7e2b0a8194ce80be9bc8ef6d3f1cbc908abbda6c
Bela VanderVoort <twobitbela@gmail.com>
2022-04-04 11:30:20 -0500

    Better breaking for casting
    closes #407

--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/CastExpression.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/CastExpression.cs
@@ -4,11 +4,19 @@ internal static class CastExpression
 {
     public static Doc Print(CastExpressionSyntax node)
     {
-        return Doc.Concat(
-            Token.Print(node.OpenParenToken),
-            Node.Print(node.Type),
-            Token.Print(node.CloseParenToken),
-            Node.Print(node.Expression)
-        );
+        // TODO review https://github.com/belav/csharpier-repos/pull/37/files for edge cases
+        return node.Expression is ParenthesizedExpressionSyntax parenthesizedExpression
+          ? Doc.Concat(
+                Token.Print(node.OpenParenToken),
+                Node.Print(node.Type),
+                Token.Print(node.CloseParenToken),
+                ParenthesizedExpression.Print(parenthesizedExpression)
+            )
+          : Doc.Group(
+                Token.Print(node.OpenParenToken),
+                Node.Print(node.Type),
+                Token.Print(node.CloseParenToken),
+                Doc.Indent(Doc.SoftLine, Node.Print(node.Expression))
+            );
     }
 }
