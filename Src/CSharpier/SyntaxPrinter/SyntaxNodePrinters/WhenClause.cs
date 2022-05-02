cd54f635dece9beccb047e63e367140b0c81e126
Bela VanderVoort <twobitbela@gmail.com>
2022-05-02 11:18:49 -0500

    Tweaking some formatting

--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/WhenClause.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/WhenClause.cs
@@ -4,15 +4,12 @@ internal static class WhenClause
 {
     public static Doc Print(WhenClauseSyntax node)
     {
-        var content = new[]
-        {
-            Doc.Line,
-            Token.PrintWithSuffix(node.WhenKeyword, " "),
-            Node.Print(node.Condition)
-        };
-
         return Doc.Group(
-            node.Parent is CasePatternSwitchLabelSyntax ? Doc.Concat(content) : Doc.Indent(content)
+            Doc.Indent(
+                Doc.Line,
+                Token.PrintWithSuffix(node.WhenKeyword, " "),
+                Node.Print(node.Condition)
+            )
         );
     }
 }
