7c89658124bf94951ec7534b2a1ceab7d1cd4659
Bela VanderVoort <twobitbela@gmail.com>
2022-05-09 10:48:49 -0500

    Fixing an edge case with properties

--- Src/CSharpier/SyntaxPrinter/MembersWithForcedLines.cs
+++ Src/CSharpier/SyntaxPrinter/MembersWithForcedLines.cs
@@ -6,13 +6,38 @@ internal static class MembersWithForcedLines
 {
     // TODO some edgecases to fix, see https://github.com/belav/csharpier-repos/pull/40/files
     // one of them is
-    //     public class ClassName
-    //     {
-    //     #if NET461
-    //         public void Blah () { }
-    //     #endif
-    //         public int Value { get; set; }
-    //     }
+/*
+public class ClassName
+{
+#if TODO
+
+
+    public void RemoveLinesAbove() { }
+#endif
+    /// <summary>Summary/summary>
+    public async Task AddLineAboveSummary() { }
+}
+
+        private sealed class WorkStealingQueue
+        {
+            /// <summary>Initial size of the queue's array.</summary>
+            private const int InitialSize = 32;
+            /// <summary>Starting index for the head and tail indices.</summary>
+            private const int StartIndex =
+#if DEBUG
+            int.MaxValue; // in debug builds, start at the end so we exercise the index reset logic
+#else
+                0;
+#endif
+            /// <summary>Head index from which to steal.  This and'd with the <see cref="_mask"/> is the index into <see cref="_array"/>.</summary>
+            private volatile int _headIndex = StartIndex
+            
+https://github.com/belav/csharpier-repos/pull/40/files#diff-7a8318af9bf1a68826755c4ef53496161290589ce1cc1edee671923480605915
+
+start from https://github.com/belav/csharpier-repos/pull/40/files#diff-cfd2c2802b10c6701ade623e79a187812b6ba0d69baa6e4a934e23354e82c103
+
+// TODO https://raw.githubusercontent.com/belav/csharpier-repos/8c770abc7626f3eb2dcbe416f2728c5cd36abfba/runtime/src/libraries/Common/src/System/Security/Cryptography/ECDsaSecurityTransforms.cs
+ */
 
     public static List<Doc> Print<T>(CSharpSyntaxNode node, IReadOnlyList<T> members)
         where T : MemberDeclarationSyntax
@@ -75,6 +100,7 @@ internal static class MembersWithForcedLines
             var printExtraNewLines = false;
             var triviaContainsEndIfOrRegion = false;
 
+            // TODO test if this even matters, or if it makes memory worse
             var leadingTrivia = member
                 .GetLeadingTrivia()
                 .Select(o => o.RawSyntaxKind())
