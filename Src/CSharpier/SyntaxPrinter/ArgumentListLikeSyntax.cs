b0288adf90825d90304bd3833359082b37d8d723
Bela VanderVoort <twobitbela@gmail.com>
2022-05-21 11:49:38 -0500

    Start of better indent for lamda
    closes #669

--- Src/CSharpier/SyntaxPrinter/ArgumentListLikeSyntax.cs
+++ Src/CSharpier/SyntaxPrinter/ArgumentListLikeSyntax.cs
@@ -9,9 +9,60 @@ internal static class ArgumentListLike
         FormattingContext context
     )
     {
+        /*
+         prettier has a lot more logic around printing arguments, we could stick to just making () => better for now
+         but it would be nice to get o => better as well, but that leads to more edge cases
+         
+        CallMethod(() =>
+        {
+            CallOtherMethod();
+        });
+
+        CallMethod(
+            (
+                longParameter_____________________________,
+                longParameter_____________________________,
+                longParameter_____________________________
+            ) =>
+            {
+                CallOtherMethod();
+            }
+        );
+
+        // this looks bad
+        var y = someList.Where(o =>
+            someLongValue_______________________
+            && theseShouldNotIndent_________________
+            && theseShouldNotIndent_________________
+                > butThisOneShould_________________________________________);
+
+        var y = someList.Where(o =>
+        {
+            return someLongValue_______________________
+                && theseShouldNotIndent_________________
+                && theseShouldNotIndent_________________
+                    > butThisOneShould_________________________________________;
+        });
+         
+         
+         */
+
         var docs = new List<Doc> { Token.Print(openParenToken, context) };
 
-        if (arguments.Any())
+        if (
+            arguments.Count == 1
+            && arguments[0].Expression
+                is ParenthesizedLambdaExpressionSyntax
+                {
+                    ParameterList: { Parameters: { Count: 0 } }
+                }
+        // TODO
+        // or SimpleLambdaExpressionSyntax
+        )
+        {
+            docs.Add(Argument.Print(arguments[0], context));
+        }
+        else if (arguments.Any())
         {
             docs.Add(
                 Doc.Indent(
