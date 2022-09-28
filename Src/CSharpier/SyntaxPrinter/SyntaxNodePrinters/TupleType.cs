0e47642a161558a3c68aa26f5f697ff8be9293e7
Bela VanderVoort <twobitbela@gmail.com>
2022-09-27 20:45:26 -0500

    Cleaning up how tuples format in more complex situations
    closes #735

--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/TupleType.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/TupleType.cs
@@ -4,9 +4,13 @@ internal static class TupleType
 {
     public static Doc Print(TupleTypeSyntax node, FormattingContext context)
     {
-        return Doc.Concat(
+        return Doc.Group(
             Token.Print(node.OpenParenToken, context),
-            SeparatedSyntaxList.Print(node.Elements, Node.Print, " ", context),
+            Doc.Indent(
+                Doc.SoftLine,
+                SeparatedSyntaxList.Print(node.Elements, Node.Print, Doc.Line, context)
+            ),
+            Doc.SoftLine,
             Token.Print(node.CloseParenToken, context)
         );
     }
