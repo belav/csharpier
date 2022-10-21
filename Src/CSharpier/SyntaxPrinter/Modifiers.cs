91f701d70f057b763054ecc7b79a17f4f9dabd2a
Guillermo Ruffino <glm.net@gmail.com>
2022-10-21 20:06:42 -0300

    use arbitrary order

--- Src/CSharpier/SyntaxPrinter/Modifiers.cs
+++ Src/CSharpier/SyntaxPrinter/Modifiers.cs
@@ -12,6 +12,19 @@ internal static class Modifiers
         return Doc.Group(Doc.Join(" ", modifiers.Select(o => Token.Print(o, context))), " ");
     }
 
+
+
+    class DefaultOrder : IComparer<string>
+    {
+        static readonly string[] DefaultOrdered = new string[] { "public", "private", "protected", "internal", "file", "static", "extern", "new", "virtual", "abstract", "sealed", "override", "readonly", "unsafe", "required", "volatile", "async" };
+        public int Compare(string? x, string? y)
+        {
+            return Array.IndexOf(DefaultOrdered, x) - Array.IndexOf(DefaultOrdered, y);
+        }
+    }
+
+    private static readonly DefaultOrder Comparer = new();
+
     public static Doc PrintWithoutLeadingTrivia(
         SyntaxTokenList modifiers,
         FormattingContext context
@@ -22,7 +35,7 @@ internal static class Modifiers
             return Doc.Null;
         }
 
-        var sortedModifiers = modifiers.OrderBy(o => o.Text);
+        var sortedModifiers = modifiers.OrderBy(o => o.Text, Comparer);
 
         return Doc.Group(
             Token.PrintWithoutLeadingTrivia(sortedModifiers.First(), context),
