509862c6758737e441ad26d501ced86052ca1e81
Domn Werner <domn.werner@gmail.com>
2022-10-24 09:09:35 -0700

    Add line when await precedes query expression (#741)
    * Add line when await precedes query expression.
    
    * Formatting.

--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/AwaitExpression.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/AwaitExpression.cs
@@ -4,9 +4,13 @@ internal static class AwaitExpression
 {
     public static Doc Print(AwaitExpressionSyntax node, FormattingContext context)
     {
+        var precedesQueryExpression = node.Expression is QueryExpressionSyntax;
+
         return Doc.Concat(
             Token.PrintWithSuffix(node.AwaitKeyword, " ", context),
-            Node.Print(node.Expression, context)
+            precedesQueryExpression
+                ? Doc.Indent(Doc.Line, Node.Print(node.Expression, context))
+                : Node.Print(node.Expression, context)
         );
     }
 }
