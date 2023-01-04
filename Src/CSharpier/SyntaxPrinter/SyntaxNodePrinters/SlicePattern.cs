4a8714c5762c921a9bcc2f66baa3e4cb9f4c679b
Bela VanderVoort <bela.vandervoort@optimizely.com>
2023-01-03 19:43:26 -0600

    fixing issue with list pattern losing code
    closes #779

--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/SlicePattern.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/SlicePattern.cs
@@ -4,6 +4,15 @@ internal static class SlicePattern
 {
     public static Doc Print(SlicePatternSyntax node, FormattingContext context)
     {
-        return Token.Print(node.DotDotToken, context);
+        if (node.Pattern is null)
+        {
+            return Token.Print(node.DotDotToken, context);
+        }
+
+        return Doc.Concat(
+            Token.Print(node.DotDotToken, context),
+            " ",
+            Node.Print(node.Pattern, context)
+        );
     }
 }
