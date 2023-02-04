c32bbfd2a169a2671a89c42869dac25aa62cdd44
Bela VanderVoort <twobitbela@gmail.com>
2023-02-04 11:51:11 -0600

    Allow empty lines in query syntax (#814)
    * Allow empty lines in query syntax
    
    closes #754
    
    * Adding some tests for removing lines

--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/JoinClause.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/JoinClause.cs
@@ -4,26 +4,29 @@ internal static class JoinClause
 {
     public static Doc Print(JoinClauseSyntax node, FormattingContext context)
     {
-        return Doc.Group(
-            Token.PrintWithSuffix(node.JoinKeyword, " ", context),
-            node.Type != null ? Doc.Concat(Node.Print(node.Type, context), " ") : Doc.Null,
-            Token.PrintWithSuffix(node.Identifier, " ", context),
-            Token.PrintWithSuffix(node.InKeyword, " ", context),
-            Node.Print(node.InExpression, context),
-            Doc.Indent(
-                Doc.Line,
-                Token.PrintWithSuffix(node.OnKeyword, " ", context),
-                Node.Print(node.LeftExpression, context),
-                " ",
-                Token.PrintWithSuffix(node.EqualsKeyword, " ", context),
-                Node.Print(node.RightExpression, context),
-                node.Into != null
-                    ? Doc.Concat(
-                        Doc.Line,
-                        Token.PrintWithSuffix(node.Into.IntoKeyword, " ", context),
-                        Token.Print(node.Into.Identifier, context)
-                    )
-                    : Doc.Null
+        return Doc.Concat(
+            ExtraNewLines.Print(node),
+            Doc.Group(
+                Token.PrintWithSuffix(node.JoinKeyword, " ", context),
+                node.Type != null ? Doc.Concat(Node.Print(node.Type, context), " ") : Doc.Null,
+                Token.PrintWithSuffix(node.Identifier, " ", context),
+                Token.PrintWithSuffix(node.InKeyword, " ", context),
+                Node.Print(node.InExpression, context),
+                Doc.Indent(
+                    Doc.Line,
+                    Token.PrintWithSuffix(node.OnKeyword, " ", context),
+                    Node.Print(node.LeftExpression, context),
+                    " ",
+                    Token.PrintWithSuffix(node.EqualsKeyword, " ", context),
+                    Node.Print(node.RightExpression, context),
+                    node.Into != null
+                        ? Doc.Concat(
+                            Doc.Line,
+                            Token.PrintWithSuffix(node.Into.IntoKeyword, " ", context),
+                            Token.Print(node.Into.Identifier, context)
+                        )
+                        : Doc.Null
+                )
             )
         );
     }
--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/SelectClause.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/SelectClause.cs
@@ -5,6 +5,7 @@ internal static class SelectClause
     public static Doc Print(SelectClauseSyntax node, FormattingContext context)
     {
         return Doc.Concat(
+            ExtraNewLines.Print(node),
             Token.PrintWithSuffix(node.SelectKeyword, " ", context),
             Node.Print(node.Expression, context)
         );
