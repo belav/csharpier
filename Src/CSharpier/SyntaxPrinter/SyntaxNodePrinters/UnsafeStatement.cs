4c32fd1d9c661bf3835fcb615a1a6259f97af197
Bela VanderVoort <bela.vandervoort@optimizely.com>
2023-06-24 09:43:27 -0500

    Keep new lines before unsafe statements
    closes #917

--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/UnsafeStatement.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/UnsafeStatement.cs
@@ -5,6 +5,7 @@ internal static class UnsafeStatement
     public static Doc Print(UnsafeStatementSyntax node, FormattingContext context)
     {
         return Doc.Concat(
+            ExtraNewLines.Print(node),
             Token.Print(node.UnsafeKeyword, context),
             Node.Print(node.Block, context)
         );
