58b2b95e9ada2d716be6ff6112a18d90ecb51d67
Rudy Peralta <rudyperalta831@gmail.com>
2023-08-16 19:56:14 -0700

    Registered range formatting for CSharpier

--- Src/CSharpier.VSCode/src/FormattingService.ts
+++ Src/CSharpier.VSCode/src/FormattingService.ts
@@ -1,5 +1,5 @@
 import { performance } from "perf_hooks";
-import { languages, Range, TextDocument, TextEdit } from "vscode";
+import { languages, Range, TextDocument, TextEdit, window } from "vscode";
 import { CSharpierProcessProvider } from "./CSharpierProcessProvider";
 import { Logger } from "./Logger";
 
@@ -11,34 +11,29 @@ export class FormattingService {
         this.logger = logger;
         this.csharpierProcessProvider = csharpierProcessProvider;
 
-        languages.registerDocumentFormattingEditProvider("csharp", {
-            provideDocumentFormattingEdits: this.provideDocumentFormattingEdits,
+        languages.registerDocumentRangeFormattingEditProvider("csharp", {
+            provideDocumentRangeFormattingEdits: this.provideDocumentRangeFormattingEdits,
         });
     }
 
-    private provideDocumentFormattingEdits = async (document: TextDocument) => {
+    private provideDocumentRangeFormattingEdits = async (document: TextDocument, range: Range) => {
         this.logger.info("Formatting started for " + document.fileName + ".");
         const startTime = performance.now();
-        const text = document.getText();
+        const text = document.getText(range);
         const newText = await this.format(text, document.fileName);
         const endTime = performance.now();
         this.logger.info("Formatted in " + (endTime - startTime) + "ms");
         if (!newText || newText === text) {
-            this.logger.debug(
-                "Skipping write because " + !newText
-                    ? "result is empty"
-                    : "current document equals result",
-            );
+            const errorMessage = "Skipping write because " + !newText
+                    ? "File is empty or selected text is an incomplete code region"
+                    : "current document equals result";
+            
+            console.warn("Error formatting document: " + errorMessage);
             return [];
         }
 
-        return [TextEdit.replace(FormattingService.fullDocumentRange(document), newText)];
+        return [TextEdit.replace(range, newText)];
     };
-
-    private static fullDocumentRange(document: TextDocument): Range {
-        const lastLineId = document.lineCount - 1;
-        return new Range(0, 0, lastLineId, document.lineAt(lastLineId).text.length);
-    }
 
     private format = async (content: string, filePath: string) => {
         return this.csharpierProcessProvider.getProcessFor(filePath).formatFile(content, filePath);
