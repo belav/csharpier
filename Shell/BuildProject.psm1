fcb136c2a91854ba52186141517f3e954d06480a
bela.vandervoort <bela.vandervoort@optimizely.com>
2023-11-05 10:47:08 -0600

    Support for net8
    closes #976

--- Shell/BuildProject.psm1
+++ Shell/BuildProject.psm1
@@ -2,7 +2,6 @@ function CSH-BuildProject
 {
     $repositoryRoot = Join-Path $PSScriptRoot ".."
     $csProjectPath = Join-Path $repositoryRoot "Src/CSharpier.Cli/CSharpier.Cli.csproj"
-    $csharpierDllPath = Join-Path $repositoryRoot "Src/CSharpier.Cli/bin/release/net7.0/dotnet-csharpier.dll"
     
     & dotnet build $csProjectPath -c release
     if ($lastExitCode -gt 0) {
