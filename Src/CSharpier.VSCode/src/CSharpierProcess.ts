71863403f7a335c76b2454cbea54c3446cef2e3b
Bela VanderVoort <twobitbela@gmail.com>
2023-11-21 12:57:06 -0600

    Fixing a couple vscode issues (#1035)
    closes #1003

--- Src/CSharpier.VSCode/src/CSharpierProcess.ts
+++ Src/CSharpier.VSCode/src/CSharpierProcess.ts
@@ -1,15 +1,24 @@
 import { Disposable } from "vscode";
+import { Logger } from "./Logger";
 
 export interface ICSharpierProcess extends Disposable {
     formatFile(content: string, filePath: string): Promise<string>;
 }
 
 export class NullCSharpierProcess implements ICSharpierProcess {
-    static instance = new NullCSharpierProcess();
+    static instance: NullCSharpierProcess;
+    private logger: Logger;
 
-    private constructor() {}
+    static create(logger: Logger) {
+        this.instance = new NullCSharpierProcess(logger);
+    }
+
+    private constructor(logger: Logger) {
+        this.logger = logger;
+    }
 
     formatFile(content: string, filePath: string): Promise<string> {
+        this.logger.debug("Skipping formatting because this is a NullCSharpierProcess. This generally indicates there was a problem starting the CSharpier process");
         return Promise.resolve("");
     }
 
