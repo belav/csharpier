76c2f73ce246783e83a5fa7d67f89397d490d9cb
Dmitry Zhavoronkov <rudomitori@gmail.com>
2023-12-13 05:27:56 +0700

    feat: Improve formatting of lambda expressions (#1066)
    # Conflicts:
    #	Src/CSharpier.Tests/FormattingTests/TestFiles/cs/MemberChains.test

--- Src/CSharpier/SyntaxPrinter/ArgumentListLikeSyntax.cs
+++ Src/CSharpier/SyntaxPrinter/ArgumentListLikeSyntax.cs
@@ -11,28 +11,70 @@ internal static class ArgumentListLike
     {
         var docs = new List<Doc> { Token.Print(openParenToken, context) };
 
-        if (
-            arguments.Count == 1
-            && arguments[0].Expression
-                is ParenthesizedLambdaExpressionSyntax
+        switch (arguments)
+        {
+            case [{ Expression: SimpleLambdaExpressionSyntax lambda } arg]:
+            {
+                docs.Add(
+                    Doc.GroupWithId(
+                        "LambdaArguments",
+                        Doc.Indent(
+                            Doc.SoftLine,
+                            Argument.PrintModifiers(arg, context),
+                            SimpleLambdaExpression.PrintHead(lambda, context)
+                        )
+                    ),
+                    Doc.IndentIfBreak(
+                        SimpleLambdaExpression.PrintBody(lambda, context),
+                        "LambdaArguments"
+                    ),
+                    lambda.Body
+                        is BlockSyntax
+                            or ObjectCreationExpressionSyntax
+                            or AnonymousObjectCreationExpressionSyntax
+                        ? Doc.IfBreak(Doc.SoftLine, Doc.Null, "LambdaArguments")
+                        : Doc.SoftLine
+                );
+                break;
+            }
+            case [
+                {
+                    Expression: ParenthesizedLambdaExpressionSyntax
                     {
                         ParameterList.Parameters.Count: 0,
                         Block: { }
-                    }
-                    or SimpleLambdaExpressionSyntax { Block: { } }
-        )
-        {
-            docs.Add(Argument.Print(arguments[0], context));
-        }
-        else if (arguments.Any())
-        {
-            docs.Add(
-                Doc.Indent(
-                    Doc.SoftLine,
-                    SeparatedSyntaxList.Print(arguments, Argument.Print, Doc.Line, context)
-                ),
-                Doc.SoftLine
-            );
+                    } lambda
+                } arg
+            ]:
+            {
+                docs.Add(
+                    Doc.GroupWithId(
+                        "LambdaArguments",
+                        Doc.Indent(
+                            Doc.SoftLine,
+                            Argument.PrintModifiers(arg, context),
+                            ParenthesizedLambdaExpression.PrintHead(lambda, context)
+                        )
+                    ),
+                    Doc.IndentIfBreak(
+                        ParenthesizedLambdaExpression.PrintBody(lambda, context),
+                        "LambdaArguments"
+                    ),
+                    Doc.IfBreak(Doc.SoftLine, Doc.Null, "LambdaArguments")
+                );
+                break;
+            }
+            default:
+            {
+                docs.Add(
+                    Doc.Indent(
+                        Doc.SoftLine,
+                        SeparatedSyntaxList.Print(arguments, Argument.Print, Doc.Line, context)
+                    ),
+                    Doc.SoftLine
+                );
+                break;
+            }
         }
 
         docs.Add(Token.Print(closeParenToken, context));
