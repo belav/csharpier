e17042532fe15b27000a9fc7b8aa17d710f9ca2e
Stefan Nikolei <stefan@nikolei.eu>
2023-12-25 17:25:50 +0100

    Update DiffEngine to latest

--- Src/CSharpier.Tests/FormattingTests/SetUpFixture.cs
+++ Src/CSharpier.Tests/FormattingTests/SetUpFixture.cs
@@ -10,16 +10,7 @@ public class SetUpFixture
     [OneTimeSetUp]
     public void OneTimeSetUp()
     {
-        EmptyFiles.Extensions.AddTextExtension(".test");
-        DiffTools.AddToolBasedOn(
-            DiffTool.WinMerge,
-            name: "WritableWinMerge",
-            arguments: (temp, target) =>
-            {
-                var leftTitle = Path.GetFileName(temp);
-                var rightTitle = Path.GetFileName(target);
-                return $"/u /wr /e \"{temp}\" \"{target}\" /dl \"{leftTitle}\" /dr \"{rightTitle}\"";
-            }
-        );
+        EmptyFiles.FileExtensions.AddTextExtension(".test");
+        DiffTools.AddToolBasedOn(DiffTool.WinMerge, name: "WritableWinMerge");
     }
 }
