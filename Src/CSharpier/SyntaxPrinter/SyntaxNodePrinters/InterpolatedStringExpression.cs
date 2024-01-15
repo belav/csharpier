b634ee33f6f42d861110920e4dfa10b62dbaae65
Bela VanderVoort <twobitbela@gmail.com>
2024-01-15 10:45:03 -0600

    fixing some issues with the recent raw string literal changes from #975 (#1106)
    * fixing some issues with the recent raw string literal changes
    
    * Getting this mostly figured out
    
    * Fixing issues with interpolated raw strings
    
    * Fixing issues with interpolated raw strings
    
    * Update InterpolatedStringExpressions.test
    
    * remove using

--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/InterpolatedStringExpression.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/InterpolatedStringExpression.cs
@@ -2,6 +2,8 @@ namespace CSharpier.SyntaxPrinter.SyntaxNodePrinters;
 
 internal static class InterpolatedStringExpression
 {
+    internal static readonly string[] lineSeparators = new[] { "\r\n", "\r", "\n" };
+
     public static Doc Print(InterpolatedStringExpressionSyntax node, FormattingContext context)
     {
         // if any of the expressions in the interpolation contain a newline then don't force this flat
@@ -13,6 +15,14 @@ internal static class InterpolatedStringExpression
                 Token.PrintLeadingTrivia(node.GetLeadingTrivia(), context),
                 node.ToString()
             );
+        }
+
+        if (
+            node.StringStartToken.RawSyntaxKind()
+            == SyntaxKind.InterpolatedMultiLineRawStringStartToken
+        )
+        {
+            return RawString(node, context);
         }
 
         var docs = new List<Doc>
@@ -28,5 +38,59 @@ internal static class InterpolatedStringExpression
             Token.PrintLeadingTrivia(node.StringStartToken, context),
             Doc.ForceFlat(docs)
         );
+    }
+
+    private static Doc RawString(InterpolatedStringExpressionSyntax node, FormattingContext context)
+    {
+        var lastLineIsIndented =
+            node.StringEndToken.Text.Replace("\r", string.Empty).Replace("\n", string.Empty)[0]
+                is '\t'
+                    or ' ';
+
+        var contents = new List<Doc>
+        {
+            Token.Print(node.StringStartToken, context),
+            lastLineIsIndented ? Doc.HardLineNoTrim : Doc.LiteralLine
+        };
+        foreach (var content in node.Contents)
+        {
+            if (content is InterpolationSyntax interpolationSyntax)
+            {
+                contents.Add(Interpolation.Print(interpolationSyntax, context));
+            }
+            else if (content is InterpolatedStringTextSyntax textSyntax)
+            {
+                if (textSyntax.TextToken.ValueText == string.Empty)
+                {
+                    continue;
+                }
+
+                var lines = textSyntax
+                    .TextToken
+                    .ValueText
+                    .Split(lineSeparators, StringSplitOptions.None);
+                for (var index = 0; index < lines.Length; index++)
+                {
+                    var line = lines[index];
+                    contents.Add(line);
+                    if (index == lines.Length - 1)
+                    {
+                        continue;
+                    }
+                    contents.Add(
+                        lastLineIsIndented
+                            ? string.IsNullOrEmpty(line)
+                                ? Doc.HardLine
+                                : Doc.HardLineNoTrim
+                            : Doc.LiteralLine
+                    );
+                }
+            }
+        }
+
+        contents.Add(lastLineIsIndented ? Doc.HardLineNoTrim : Doc.LiteralLine);
+        contents.Add(Token.Print(node.StringEndToken, context));
+
+        return Doc.IndentIf(node.Parent is not ArgumentSyntax, Doc.Concat(contents));
     }
 }
