e6209b2d404cbd3542959eb399bc00c0dfe560b1
Alan Chung Ma <alanchunggt@gmail.com>
2024-01-23 21:30:39 -0500

    Removed hash from version in dotnet tools (#1138)
    Co-authored-by: Bela VanderVoort <twobitbela@gmail.com>

--- Src/CSharpier.Rider/src/main/java/com/intellij/csharpier/CSharpierProcessProvider.java
+++ Src/CSharpier.Rider/src/main/java/com/intellij/csharpier/CSharpierProcessProvider.java
@@ -137,6 +137,10 @@ public class CSharpierProcessProvider implements DocumentListener, Disposable, I
                         if (csharpier != null) {
                             var version = csharpier.get("version").getAsString();
                             if (version != null) {
+                                if (version.Contains("+"))
+                                {
+                                    version = version.split(Pattern.quote("+"))[0]
+                                }
                                 this.logger.debug("Found version " + version + " in " + dotnetToolsPath);
                                 return version;
                             }
