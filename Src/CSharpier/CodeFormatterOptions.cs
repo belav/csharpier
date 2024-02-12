de502caced094c9af359dce39330172814a99353
Casper Lindschouw <casper@lindschouw.me>
2024-02-12 19:36:33 +0100

    Sync exposed CodeFormatterOptions with currently supported cli options
    Closes #1172

--- Src/CSharpier/CodeFormatterOptions.cs
+++ Src/CSharpier/CodeFormatterOptions.cs
@@ -3,4 +3,13 @@ namespace CSharpier;
 public class CodeFormatterOptions
 {
     public int Width { get; init; } = 100;
+    public IndentStyle IndentStyle { get; init; } = IndentStyle.Spaces;
+    public int IndentSize { get; init; } = 4;
+    public EndOfLine EndOfLine { get; init; } = EndOfLine.Auto;
+}
+
+public enum IndentStyle
+{
+    Spaces,
+    Tabs
 }
