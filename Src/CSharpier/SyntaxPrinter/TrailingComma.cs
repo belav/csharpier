04ce0024778dc3931e6a230d038e5b511102da7c
Bela VanderVoort <twobitbela@gmail.com>
2024-08-13 18:39:42 -0500

    Fixing issue with trailing commas that have a trailing comment (#1301)
    closes #1300
    
    ---------
    
    Co-authored-by: Lasath Fernando <devel@lasath.org>

--- Src/CSharpier/SyntaxPrinter/TrailingComma.cs
+++ Src/CSharpier/SyntaxPrinter/TrailingComma.cs
@@ -2,13 +2,13 @@ namespace CSharpier.SyntaxPrinter;
 
 internal static class TrailingComma
 {
-    public static Doc Print(SyntaxToken nextSyntaxToken, FormattingContext context)
+    public static Doc Print(SyntaxToken closingToken, FormattingContext context)
     {
-        return !nextSyntaxToken.LeadingTrivia.Any(o => o.IsDirective)
-            ? Doc.IfBreak(
+        return closingToken.LeadingTrivia.Any(o => o.IsDirective)
+            ? Doc.Null
+            : Doc.IfBreak(
                 Token.Print(SyntaxFactory.Token(SyntaxKind.CommaToken), context),
                 Doc.Null
-            )
-            : Doc.Null;
+            );
     }
 }
