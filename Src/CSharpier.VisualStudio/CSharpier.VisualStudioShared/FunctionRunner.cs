61ad5011f33eca8bc583137927c19693103af64c
Bela VanderVoort <bela.vandervoort@optimizely.com>
2024-08-18 14:27:48 -0500

    Use dotnet tool list to find both local and global installs of csharpier.
    #1267

---
+++ Src/CSharpier.VisualStudio/CSharpier.VisualStudioShared/FunctionRunner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharpier.VisualStudio
+{
+    using System.Linq;
+
+    public static class FunctionRunner
+    {
+        public static string? RunUntilNonNull(params Func<string?>[] functions)
+        {
+            return functions
+                .Select(possibleFunction => possibleFunction())
+                .OfType<string>()
+                .FirstOrDefault();
+        }
+    }
+}
