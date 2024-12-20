89320d02a36dca9f078a1908382b91957cddabab
Bela VanderVoort <bela.vandervoort@optimizely.com>
2024-12-20 10:57:07 -0600

    Figuring out a way to prevent the extra line before a file ending comment
    closes #1408

--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/CompilationUnit.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/CompilationUnit.cs
@@ -14,8 +14,24 @@ internal static class CompilationUnit
         );
         if (finalTrivia != Doc.Null)
         {
-            // even though we include the initialNewLines above, a literalLine from directives trims the hardline, so add an extra one here
-            docs.Add(Doc.HardLineIfNoPreviousLine, finalTrivia);
+            // really ugly code to prevent a comment at the end of a file from continually inserting new blank lines
+            if (
+                finalTrivia is Concat { Contents.Count: > 1 } list
+                && list.Contents[1] is LeadingComment
+                && docs[^1] is Concat previousList
+                && previousList.Contents[^1] is HardLine
+                && previousList.Contents[^2] is HardLine
+            )
+            {
+                list.Contents.RemoveAt(0);
+
+                docs.Add(finalTrivia);
+            }
+            else
+            {
+                // even though we include the initialNewLines above, a literalLine from directives trims the hardline, so add an extra one here
+                docs.Add(Doc.HardLineIfNoPreviousLine, finalTrivia);
+            }
         }
         docs.Add(Doc.HardLineIfNoPreviousLine);
 
