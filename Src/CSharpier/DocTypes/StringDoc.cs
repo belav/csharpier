999674eb576ff4340cbedbbfffef3d2a4c69f1a1
Timothy Makkison <timmakkison@gmail.com>
2025-02-19 23:04:03 +0000

    feat: cache common `StringDoc` types

--- Src/CSharpier/DocTypes/StringDoc.cs
+++ Src/CSharpier/DocTypes/StringDoc.cs
@@ -4,4 +4,39 @@ internal sealed class StringDoc(string value, bool isDirective = false) : Doc
 {
     public string Value { get; } = value;
     public bool IsDirective { get; } = isDirective;
+
+    public static StringDoc Create(string value)
+    {
+        if (value.Length != 1)
+        {
+            return new StringDoc(value);
+        }
+
+        return value switch
+        {
+            " " => SpaceString,
+            "\t" => TabString,
+            "," => CommaString,
+            "=" => EqualsString,
+            "." => DotString,
+            "{" => OpenBraceString,
+            "}" => ClosedBraceString,
+            "(" => OpenBracketString,
+            ")" => ClosedBracketString,
+            ";" => SemiColonString,
+
+            _ => new StringDoc(value),
+        };
+    }
+
+    private static readonly StringDoc SpaceString = new(" ");
+    private static readonly StringDoc TabString = new("\t");
+    private static readonly StringDoc CommaString = new(",");
+    private static readonly StringDoc EqualsString = new("=");
+    private static readonly StringDoc DotString = new(".");
+    private static readonly StringDoc OpenBraceString = new("{");
+    private static readonly StringDoc ClosedBraceString = new("}");
+    private static readonly StringDoc OpenBracketString = new("(");
+    private static readonly StringDoc ClosedBracketString = new(")");
+    private static readonly StringDoc SemiColonString = new(";");
 }
