76a7c4b9138e545dd008468fc00f8728138b72de
moormaster <moormaster@gmx.net>
2025-03-03 19:22:04 +0100

    refactor: simplified FileIssueLogger.WriteError()

--- Src/CSharpier.Cli/FileIssueLogger.cs
+++ Src/CSharpier.Cli/FileIssueLogger.cs
@@ -4,27 +4,12 @@ namespace CSharpier.Cli;
 
 internal class FileIssueLogger(string filePath, ILogger logger, bool isMsBuildFormat)
 {
-    static string FormatMsBuildError(LogState logState, Exception? exception) =>
-        $"{logState.Path}: error: {logState.Message}";
-
-    record class LogState
-    {
-        public required string Path;
-        public required string Message;
-    }
-
     public void WriteError(string value, Exception? exception = null)
     {
         if (isMsBuildFormat)
-        {
-            var logState = new LogState() { Path = this.GetPath(), Message = value };
-
-            logger.Log(LogLevel.Error, 0, logState, exception, FormatMsBuildError);
-        }
+            logger.LogError("{Path}: error: {Message}", this.GetPath(), value);
         else
-        {
             logger.LogError(exception, $"{this.GetPath()} - {value}");
-        }
     }
 
     public void WriteWarning(string value)
