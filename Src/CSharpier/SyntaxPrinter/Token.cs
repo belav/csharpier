belav/prettier-plugin-csharpier

perf: cache common `StringDoc` types

Src/CSharpier/SyntaxPrinter/Token.cs
@@ -118,7 +118,7 @@ internal static class Token
         }
         else
         {
-            docs.Append(syntaxToken.Text);
+            docs.Append(StringDoc.ToStringDoc(syntaxToken));
         }
 
         if (!skipTrailingTrivia)

belav/csharpier

Src/CSharpier/SyntaxPrinter/Token.cs
@@ -118,7 +118,7 @@ internal static class Token
         }
         else
         {
-            docs.Append(syntaxToken.Text);
+            docs.Append(StringDoc.Create(syntaxToken));
         }
 
         if (!skipTrailingTrivia)