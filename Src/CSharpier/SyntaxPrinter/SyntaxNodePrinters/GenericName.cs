4ffa60f47eb396417d556c89453a8376af55895b
Bela VanderVoort <bela.vandervoort@optimizely.com>
2025-04-04 12:35:17 -0500

    fixing issue with comment on generic causing member chain to break
    closes #1555

--- Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/GenericName.cs
+++ Src/CSharpier/SyntaxPrinter/SyntaxNodePrinters/GenericName.cs
@@ -4,9 +4,12 @@ internal static class GenericName
 {
     public static Doc Print(GenericNameSyntax node, PrintingContext context)
     {
-        return Doc.Group(
-            Token.Print(node.Identifier, context),
-            Node.Print(node.TypeArgumentList, context)
+        return Doc.Concat(
+            Token.PrintLeadingTrivia(node.Identifier, context),
+            Doc.Group(
+                Token.PrintWithoutLeadingTrivia(node.Identifier, context),
+                Node.Print(node.TypeArgumentList, context)
+            )
         );
     }
 }
