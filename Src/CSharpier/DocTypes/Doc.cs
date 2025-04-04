belav/prettier-plugin-csharpier

perf: cache common `StringDoc` types

Src/CSharpier/DocTypes/Doc.cs
@@ -12,10 +12,7 @@ internal abstract class Doc
         return DocSerializer.Serialize(this);
     }
 
-    public static implicit operator Doc(string value)
-    {
-        return new StringDoc(value);
-    }
+    public static implicit operator Doc(string value) => StringDoc.ToStringDoc(value);
 
     public static NullDoc Null => NullDoc.Instance;
 

Src/CSharpier/SyntaxPrinter/Token.cs
@@ -118,7 +118,7 @@ internal static class Token
         }
         else
         {
-            docs.Append(syntaxToken.Text);
+            docs.Append(StringDoc.ToStringDoc(syntaxToken));
         }
 
         if (!skipTrailingTrivia)

belav/csharpier

Src/CSharpier/DocTypes/Doc.cs
@@ -12,10 +12,7 @@ internal abstract class Doc
         return DocSerializer.Serialize(this);
     }
 
-    public static implicit operator Doc(string value)
-    {
-        return new StringDoc(value);
-    }
+    public static implicit operator Doc(string value) => StringDoc.Create(value);
 
     public static NullDoc Null => NullDoc.Instance;
 

Src/CSharpier/SyntaxPrinter/Token.cs
@@ -116,7 +116,7 @@ internal static class Token
         }
         else
         {
-            docs.Append(syntaxToken.Text);
+            docs.Append(StringDoc.Create(syntaxToken));
         }
 
         if (!skipTrailingTrivia)