6f2164a5dffb8578dd006c4f77c61327932b00ba
Bela VanderVoort <bela.vandervoort@optimizely.com>
2025-04-19 12:53:44 -0500

    getting tool command back to what it was

--- Shell/BuildPackages.psm1
+++ Shell/BuildPackages.psm1
@@ -2,7 +2,7 @@ function CSH-BuildPackages() {
     $repositoryRoot = Join-Path $PSScriptRoot ".."
 
     # build individual projects because CSharpier.MSBuild can't be in the sln
-    $projects = @("CSharpier.Cli", "CSharpier", "CSharpier.MSBuild")
+    $projects = @("CSharpier.Cli", "CSharpier.Core", "CSharpier.MSBuild")
     $nupkgPath = Join-Path $repositoryRoot "nupkg"
 
     foreach ($project in $projects) {
