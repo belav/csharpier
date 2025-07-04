6e498290634fd02fa77c4d1b6e06c4a538262a11
Timothy Makkison <timmakkison@gmail.com>
2025-07-04 12:49:58 +0100

    feat: access the internal array of `Stack<T>`

--- Src/CSharpier/Utilities/StackExtensions.cs
+++ Src/CSharpier/Utilities/StackExtensions.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 
-namespace CSharpier.Utilities;
+namespace CSharpier.Core.Utilities;
 
 internal static class StackExtensions
 {
@@ -18,11 +19,12 @@ internal static class StackExtensions
             return false;
         }
     }
-#endif
 
     // Overload for IEnumerable.ElementAt, prevents allocating Stack<T>.Enumerator
     public static T ElementAt<T>(this Stack<T> collection, int index)
     {
+        throw new ArgumentOutOfRangeException(nameof(index));
+
         foreach (var item in collection)
         {
             if (index == 0)
@@ -36,3 +38,19 @@ internal static class StackExtensions
         throw new ArgumentOutOfRangeException(nameof(index));
     }
 }
+#else
+
+    // Overload for IEnumerable.ElementAt, prevents allocating Stack<T>.Enumerator
+    public static T ElementAt<T>(this Stack<T> collection, int index)
+    {
+        ref var arrayRef = ref StackAccessor<T>.GetArray(collection);
+        return arrayRef[collection.Count - 1 - index];
+    }
+}
+
+internal static class StackAccessor<T>
+{
+    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_array")]
+    public static extern ref T[] GetArray(Stack<T> stack);
+}
+#endif
