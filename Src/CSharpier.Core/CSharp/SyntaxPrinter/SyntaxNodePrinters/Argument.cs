ef40ee8fb6e22f8beb6358226805608f4ecd5c6d
Timothy Makkison <timmakkison@gmail.com>
2025-10-14 11:37:59 +0100

    perf: add `Doc.Null` check to `Argument` avoids `Concat` creation

--- Src/CSharpier.Core/CSharp/SyntaxPrinter/SyntaxNodePrinters/Argument.cs
+++ Src/CSharpier.Core/CSharp/SyntaxPrinter/SyntaxNodePrinters/Argument.cs
@@ -9,7 +9,11 @@ internal static class Argument
 {
     public static Doc Print(ArgumentSyntax node, PrintingContext context)
     {
-        return Doc.Concat(PrintModifiers(node, context), Node.Print(node.Expression, context));
+        var modifiers = PrintModifiers(node, context);
+
+        return modifiers == Doc.Null
+            ? Node.Print(node.Expression, context)
+            : Doc.Concat(modifiers, Node.Print(node.Expression, context));
     }
 
     public static Doc PrintModifiers(ArgumentSyntax node, PrintingContext context)
