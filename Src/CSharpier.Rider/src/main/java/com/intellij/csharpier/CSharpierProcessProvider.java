4eb155ef710bf4680d2160929fb9b844020c4e17
Robert Dailey <git@rdailey.me>
2026-02-23 11:22:46 -0600

    fix: replace Rider plugin .gitignore with gitignore.io templates
    Use gradle,kotlin,jetbrains+all templates from gitignore.io to get
    comprehensive coverage of build artifacts. Add .qodana and .settings/
    which aren't in the templates but are generated by the Gradle
    Buildship/Eclipse plugin.

--- Src/CSharpier.Rider/src/main/java/com/intellij/csharpier/CSharpierProcessProvider.java
+++ Src/CSharpier.Rider/src/main/java/com/intellij/csharpier/CSharpierProcessProvider.java
@@ -207,11 +207,15 @@ public class CSharpierProcessProvider implements DocumentListener, Disposable, I
 
     private String findVersionInCsProj(Path currentDirectory) {
         this.logger.debug("Looking for " + currentDirectory + "/*.csproj");
-        File[] csProjFiles = currentDirectory.toFile()
+        File[] csProjFiles = currentDirectory
+            .toFile()
             .listFiles((dir, name) -> name.toLowerCase().endsWith(".csproj"));
         if (csProjFiles == null) {
-            this.logger.debug("Unable to list files in directory: " + currentDirectory +
-                " (directory may not exist, not be a directory, or be temporarily inaccessible)");
+            this.logger.debug(
+                    "Unable to list files in directory: " +
+                    currentDirectory +
+                    " (directory may not exist, not be a directory, or be temporarily inaccessible)"
+                );
             return null;
         }
         for (var pathToCsProj : csProjFiles) {
