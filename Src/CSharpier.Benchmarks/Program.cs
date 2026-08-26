237fa7207dc113f10b6baa6496e40564f0fa9b5e
Bela VanderVoort <bela.vandervoort@optimizely.com>
2026-08-25 21:44:11 -0500

    Update benchmarks

--- Src/CSharpier.Benchmarks/Program.cs
+++ Src/CSharpier.Benchmarks/Program.cs
@@ -1,13 +1,4 @@
 ﻿using BenchmarkDotNet.Running;
 using CSharpier.Benchmarks;
 
-if (args is { Length: > 0 })
-{
-    _ = BenchmarkSwitcher.FromAssembly(typeof(CSharpBenchmarks).Assembly).Run(args);
-}
-else
-{
-    _ = BenchmarkRunner.Run<CSharpBenchmarks>();
-    // _ = BenchmarkRunner.Run<CliBenchmarks>();
-    // _ = BenchmarkRunner.Run<XmlBenchmarks>();
-}
+BenchmarkSwitcher.FromAssembly(typeof(CliBenchmarks).Assembly).Run(args);
